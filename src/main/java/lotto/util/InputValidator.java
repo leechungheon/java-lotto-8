bce267a2e40469b57a5d2d7e1aa8aedb09f4aeff
leechungheon <leechung6311@gmail.com>
2025-11-01 19:51:07 +0900

    refactor(InputView): inputPurchaseAmount 메서드 검증 로직 분리

---
+++ src/main/java/lotto/util/InputValidator.java
@@ -0,0 +1,19 @@
+package lotto.util;
+
+import java.util.List;
+
+public class InputValidator {
+    private static final int PURCHASE_AMOUNT_UNIT = 1000;
+    public static void validateInputPurchaseAmount(int purchaseAmount){
+        if(purchaseAmount<=0){
+            throw new IllegalArgumentException("[ERROR] 0원 이하의 금액은 입력할 수 없습니다.");
+        }
+        if(purchaseAmount % PURCHASE_AMOUNT_UNIT != 0){
+            throw new IllegalArgumentException("[ERROR] 금액을 1,000원 단위로 입력해주세요.");
+        }
+    }
+
+    public static void validateInputWinningNumbers(List<Integer> winningNumbers){
+
+    }
+}
--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -2,19 +2,13 @@ package lotto.view;
 
 import camp.nextstep.edu.missionutils.Console;
 import java.util.*;
+import lotto.util.InputValidator;
 
 public class InputView {
-    // 검증로직 분리하여 메서드 길이 줄이기
     public int inputPurchaseAmount(){
         try{
             int purchaseAmount = Integer.parseInt(Console.readLine());
-
-            if(purchaseAmount<=0){
-                throw new IllegalArgumentException("[ERROR] 0원 이하의 금액은 입력할 수 없습니다.");
-            }
-            if(purchaseAmount%1000 != 0){
-                throw new IllegalArgumentException("[ERROR] 금액을 1,000원 단위로 입력해주세요.");
-            }
+            InputValidator.validateInputPurchaseAmount(purchaseAmount);
             return purchaseAmount;
         }catch(NumberFormatException e){
            throw new IllegalArgumentException("[ERROR] 숫자를 입력해주세요.");
@@ -22,13 +16,14 @@ public class InputView {
     }
 
     public List<Integer> inputWinningNumbers(){
-        String winningNumbers = Console.readLine();
-        // 검증 메서드 추후 구현
         try {
-            return Arrays.stream(winningNumbers.split(","))
+            String s = Console.readLine();
+            List<Integer> winningNumbers = Arrays.stream(s.split(","))
                     .map(String::trim)
                     .map(Integer::parseInt)
                     .toList();
+            InputValidator.validateInputWinningNumbers(winningNumbers);
+            return winningNumbers;
         }catch(NumberFormatException e){
             throw new IllegalArgumentException("[ERROR] 숫자를 입력해주세요.");
         }
